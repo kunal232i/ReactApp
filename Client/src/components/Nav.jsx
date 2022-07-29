39b7e2ecd84897ce5fddaf6a61730ac06f2675b3
kunal232i <kingawale23@gmail.com>
2022-07-29 09:53:11 +0000

    initial commit

---
+++ Client/src/components/Nav.jsx
@@ -0,0 +1,7 @@
+import "./App.css";
+import nav from ".style/nav";
+const Nav = () => {
+  return <h1 className="nav">Noty</h1>;
+};
+
+export default Nav;
---
+++ Client/src/components/Searchbar.jsx
@@ -0,0 +1,11 @@
+import "../style/searchBar.style.css";
+
+const SearchBar = () => {
+  return (
+    <div className="search-bar">
+      <input type="text" placeholder="🔍" />
+    </div>
+  );
+};
+
+export default SearchBar;
